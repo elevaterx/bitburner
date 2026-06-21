6a6a86fdf076b2f86a779bb1420eddcdc7041d35
Avruch0101 <nawrich@gmail.com>
2026-06-21 10:18:52 -0400

    Delete w.js

--- w.js
+++
@@ -1,9 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-    const t = ns.args[0];
-    while (true) {
-        if (ns.getServerSecurityLevel(t) > ns.getServerMinSecurityLevel(t) + 1) await ns.weaken(t);
-        else if (ns.getServerMoneyAvailable(t) < ns.getServerMaxMoney(t) * 0.99) await ns.grow(t);
-        else await ns.sleep(500);
-    }
-}
