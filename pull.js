72082d106e335079d3e451e441a39482702fb271
Avruch0101 <nawrich@gmail.com>
2026-06-22 23:12:19 -0400

    Update pull.js

--- pull.js
+++ pull.js
@@ -15,7 +15,7 @@ export async function main(ns) {
         "batch-math.js", "batch-live.js",
         "bhack.js", "bgrow.js", "bweaken.js",
         "bprep.js", "bdiag.js", "bbatch.js",
-        "pull.js", "killfarm.js"
+        "pull.js", "killfarm.js", "bbatch2.js"
     ];
     const base = "https://raw.githubusercontent.com/" + USER + "/" + REPO + "/" + BRANCH + "/";
     const running = new Set();
