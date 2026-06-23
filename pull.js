1919b1d90e30f894c171a3b0791570297ade07d7
Avruch0101 <nawrich@gmail.com>
2026-06-23 18:15:53 -0400

    Update pull.js

--- pull.js
+++ pull.js
@@ -11,7 +11,7 @@ export async function main(ns) {
         "puzzles.js", "purchaser.js", "cleanup.js",
         "xp.js", "xpfarm.js", "sh.js", "shareall.js",
         "diagnose-income.js", "earners.js", "backdoors.js",
-        "sing.js", "hacknet.js",
+        "sing.js", "hacknet.js", "hud1.js", "hud2.js", "cloudstat.js", "fatcheck.js"
         // --- HWGW batcher ---
         "batch-math.js", "batch-live.js", "sharecap.js",
         "bhack.js", "bgrow.js", "bweaken.js",
